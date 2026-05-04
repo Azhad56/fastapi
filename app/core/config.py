a0eec5bd914fa2413b2b8797195a197dce24faf8
Azhad Ghufran <azhad.16510@sscbs.du.ac.in>
2026-05-04 16:56:52 +0530

    add env file

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,20 @@
 from pydantic_settings import BaseSettings
+from functools import lru_cache
+
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:admin@localhost:5432/ecommerce"
-    SECRET_KEY: str = "supersecret"
-    ALGORITHM: str = "HS256"
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ENVIRONMENT: str
 
     class Config:
         env_file = ".env"
+        case_sensitive = True
 
-settings = Settings()
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+settings = get_settings()
+
